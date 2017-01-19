5f0d33a4c613a2817e220d9f6b55afeb9c100cc7
harshit211997 <harshit211997@gmail.com>
2017-01-20 00:45:29 +0530

    Initial commit

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	public float speed = 5f;
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.AddForce ((Vector3.right + Vector3.up) * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
